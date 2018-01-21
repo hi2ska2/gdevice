b7c8b3f9bd72ba817305805cc31f70ef187d0270
Sung-Min Hong <smhong@gist.ac.kr>
2018-01-21 20:24:43 +0900

    Add missing files

---
+++ World.cc
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////
+// G-Device                                   //
+//                                            //
+// Copyright 2018 Sung-Min Hong               //
+//                                            //
+// Please see LICENSE for license information //
+////////////////////////////////////////////////
+
+// World.cc
+
+#include "World.h"
+
+World::World()
+{
+  //
+}
---
+++ World.h
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////
+// G-Device                                   //
+//                                            //
+// Copyright 2018 Sung-Min Hong               //
+//                                            //
+// Please see LICENSE for license information //
+////////////////////////////////////////////////
+
+// World.h
+
+#ifndef WorldH
+#define WorldH
+
+class World {
+ public:
+  World();
+
+ private:
+  //
+};
+
+#endif
