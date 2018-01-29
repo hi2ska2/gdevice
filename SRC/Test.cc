47bbf778c8d268be198bfe5dd1c071c8609ebac9
Sung-Min Hong <smhong@gist.ac.kr>
2018-01-29 10:15:24 +0900

    Create Test.cc and Test.h

---
+++ SRC/Test.cc
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////
+// G-Device                                    //
+//                                             //
+// Copyright 2018 Sung-Min Hong                //
+//                                             //
+// Please see LICENSE for license information. //
+/////////////////////////////////////////////////
+
+// Test.cc
+
+#include "Test.h"
+
+#include "utilities.h"
+
+Test::Test(const std::string& name)
+  : Thing(name,ThingType::LUMPED) 
+{
+  // Nothing to do
+}
