563f9f34c02988faae1dc335d1927a2438b77eb6
SungHyunCha <shcha@gist.ac.kr>
2018-01-29 17:10:22 +0900

    Create DGFET.cc and DGFET.h

---
+++ SRC/DGFET.h
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////
+// G-Device                                    //
+//                                             //
+// Copyright 2018 Sung-Min Hong                //
+//                                             //
+// Please see LICENSE for license information. //
+/////////////////////////////////////////////////
+
+// DGFET.h
+
+#ifndef DGFETH
+#define DGFETH
+
+#include "Thing.h"
+
+class DGFET : public Thing {
+ public:
+  DGFET(const std::string& name);
+
+ private:
+  // Nothing inside  
+};
+  
+
+#endif
