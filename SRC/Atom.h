bab7a197d1faeef65657b81fa69e5367b1296977
quasar <nslyers4018@gmail.com>
2018-01-29 19:50:59 +0900

    Add Atom.cc and Atom.h

---
+++ SRC/Atom.h
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////
+// G-Device                                    //
+//                                             //
+// Copyright 2018 Sung-Min Hong                //
+//                                             //
+// Please see LICENSE for license information. //
+/////////////////////////////////////////////////
+
+// Atom.h
+
+#ifndef AtomH
+#define AtomH
+
+#include "Thing.h"
+
+class Atom : public Thing {
+public:
+    Atom(const std::string& name);
+
+private:
+    // Nothing inside
+};
+
+
+#endif
